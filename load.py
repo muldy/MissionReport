831f16b23188b0c6560e8dda0c2ce6c0659f3a76
muldy <muldy@udesktop>
2018-02-12 13:10:59 +0000

    static content for webserver

--- load.py
+++ load.py
@@ -5,10 +5,10 @@ import Tkinter as tk
 import myNotebook as nb
 from config import config
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/static')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
@@ -18,9 +18,17 @@ def index():
    """Serve the client-side application."""
    return render_template('index.html')
 
+@app.route('/js/<path:path>')
+def send_js(path):
+        return send_from_directory('js', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+        return send_from_directory('js', path)
+
 
 def flaskThread():
-    app.run()
+    app.run(host='0.0.0.0.',port=8666)
 
 def plugin_start():
    """
