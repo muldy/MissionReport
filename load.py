55905cb2806bebdf08c12ada0c27d1a1d963823d
muldy <muldy@udesktop>
2018-02-13 12:20:35 +0000

    thread safe?!

--- load.py
+++ load.py
@@ -17,128 +17,139 @@ app = Flask(__name__,static_url_path='/static')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thready = None
+flask_thread= None
 thread_lock = Lock()
 
 @app.route('/')
 def index():
-   """Serve the client-side application."""
-   return render_template('index.html', async_mode=socketio.async_mode)
+	"""Serve the client-side application."""
+	return render_template('index.html', async_mode=socketio.async_mode)
 
 def background_thread():
-    """Example of how to send server generated events to clients."""
-    count = 0
-    while True:
-        socketio.sleep(10)
-        count += 1
-        socketio.emit('my_response',
-                      {'data': 'Server generated event', 'count': count},
-                      namespace='/test')
+	"""Example of how to send server generated events to clients."""
+	count = 0
+	while True:
+		socketio.sleep(10)
+		count += 1
+		socketio.emit('my_response',
+			{'data': 'Server generated event', 'count': count},
+			namespace='/test')
 
 @socketio.on('my_event', namespace='/test')
 def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
+	session['receive_count'] = session.get('receive_count', 0) + 1
+	emit('my_response',
+		{'data': message['data'], 'count': session['receive_count']})
 
 
 @socketio.on('my_broadcast_event', namespace='/test')
 def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
+	session['receive_count'] = session.get('receive_count', 0) + 1
+	emit('my_response',
+		{'data': message['data'], 'count': session['receive_count']},
+		broadcast=True)
 
 
 @socketio.on('join', namespace='/test')
 def join(message):
-    join_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
+	join_room(message['room'])
+	session['receive_count'] = session.get('receive_count', 0) + 1
+	emit('my_response',
+		{'data': 'In rooms: ' + ', '.join(rooms()),
+		'count': session['receive_count']})
 
 
 @socketio.on('leave', namespace='/test')
 def leave(message):
-    leave_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
+	leave_room(message['room'])
+	session['receive_count'] = session.get('receive_count', 0) + 1
+	emit('my_response',
+		{'data': 'In rooms: ' + ', '.join(rooms()),
+		'count': session['receive_count']})
 
 
 @socketio.on('close_room', namespace='/test')
 def close(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
-         room=message['room'])
-    close_room(message['room'])
+	session['receive_count'] = session.get('receive_count', 0) + 1
+	emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
+	'count': session['receive_count']},
+	room=message['room'])
+	close_room(message['room'])
 
 
 @socketio.on('my_room_event', namespace='/test')
 def send_room_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         room=message['room'])
+	session['receive_count'] = session.get('receive_count', 0) + 1
+	emit('my_response',
+		{'data': message['data'], 'count': session['receive_count']},
+		room=message['room'])
 
 
 @socketio.on('disconnect_request', namespace='/test')
 def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']})
-    disconnect()
+	session['receive_count'] = session.get('receive_count', 0) + 1
+	emit('my_response',
+		{'data': 'Disconnected!', 'count': session['receive_count']})
+	disconnect()
 
 
 @socketio.on('my_ping', namespace='/test')
 def ping_pong():
-    print("PONG!")
-    emit('my_pong')
+	emit('my_pong')
 
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    global thready
-    with thread_lock:
-        if thready is None:
-            thready = socketio.start_background_task(target=background_thread)
-    emit('my_response', {'data': 'Connected', 'count': 0})
+	global thready
+	with thread_lock:
+		if thready is None:
+			thready = socketio.start_background_task(target=background_thread)
+	emit('my_response', {'data': 'Connected', 'count': 0})
 
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
-    print('Client disconnected', request.sid)
+	print('Client disconnected', request.sid)
 
 def flaskThread():
-    #app.run(host='0.0.0.0.',port=8666)
-    socketio.run(app,host='0.0.0.0.',port=8666)
+	#app.run(host='0.0.0.0.',port=8666)
+	socketio.run(app,host='0.0.0.0.',port=8666)
 
 def plugin_start():
-   """
-   Load this plugin into EDMC
-   """
-   thread.start_new_thread(flaskThread,())
-   print "MissionReport Loaded"
-   return "Mission Report"
+	"""
+	Load this plugin into EDMC
+	"""
+	flask_thread=thread.start_new_thread(flaskThread,())
+	print "MissionReport Loaded"
+	return "Mission Report"
 
 def plugin_stop():
-    """
-    EDMC is closing
-    """
-    print "MissionReport unloaded!"
+	"""
+	EDMC is closing
+	"""
+	with thread_lock:
+		if thready is not None:
+			print "stop threads"
+			thready.stop()
+			flask_thread.stop()
+	print "MissionReport unloaded!"
 
 
 
 def plugin_prefs(parent, cmdr, is_beta):
-   """
-   Return a TK Frame for adding to the EDMC settings dialog.
-   """
-   this.mysetting = tk.IntVar(value=config.getint("MyPluginSetting"))	# Retrieve saved value from config
-   frame = nb.Frame(parent)
-   nb.Label(frame, text="Hello").grid()
-   nb.Label(frame, text="Commander").grid()
-   nb.Checkbutton(frame, text="My Setting", variable=this.mysetting).grid()
+	"""
+	Return a TK Frame for adding to the EDMC settings dialog.
+	"""
+	this.mysetting = tk.IntVar(value=config.getint("MyPluginSetting"))	# Retrieve saved value from config
+	frame = nb.Frame(parent)
+	nb.Label(frame, text="Hello").grid()
+	nb.Label(frame, text="Commander").grid()
+	nb.Checkbutton(frame, text="My Setting", variable=this.mysetting).grid()
 
-   return frame
+	return frame
+
+def prefs_changed(cmdr, is_beta):
+	"""
+	Save settings.
+	"""
+	config.set('MyPluginSetting', this.mysetting.getint())	# Store new value in config
