1c53247637da2f9e3194582c9fb70b4752caa58f
muldy <muldy@rogue01>
2018-02-26 10:28:09 +0000

    cleanup

--- load.py
+++ load.py
@@ -15,12 +15,9 @@ import json
 async_mode = None
 
 app = Flask(__name__,static_url_path='/static')
-app.config['SECRET_KEY'] = 'secret!'
+#app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)#, message_queue='redis://')
-#socketio_c = SocketIO(message_queue='redis://')
-thready = None
 flask_thread= None
-thread_lock = Lock()
 
 this = sys.modules[__name__]	# For holding module globals
 this.server_local=1
@@ -34,95 +31,17 @@ def index():
 	"""Serve the client-side application."""
 	return render_template('index.html', async_mode=socketio.async_mode)
 
-def background_thread():
-	"""Example of how to send server generated events to clients."""
-	count = 0
-	while True:
-		socketio.sleep(10)
-		count += 1
-		socketio.emit('my_response',
-			{'data': 'Server generated event', 'count': count},
-			namespace='/test')
-
-@socketio.on('my_event', namespace='/test')
-def test_message(message):
-	session['receive_count'] = session.get('receive_count', 0) + 1
-	emit('my_response',
-		{'data': message['data'], 'count': session['receive_count']})
-
-
-@socketio.on('my_broadcast_event', namespace='/test')
-def test_broadcast_message(message):
-	session['receive_count'] = session.get('receive_count', 0) + 1
-	emit('my_response',
-		{'data': message['data'], 'count': session['receive_count']},
-		broadcast=True)
-
-
-@socketio.on('join', namespace='/test')
-def join(message):
-	join_room(message['room'])
-	session['receive_count'] = session.get('receive_count', 0) + 1
-	emit('my_response',
-		{'data': 'In rooms: ' + ', '.join(rooms()),
-		'count': session['receive_count']})
-
-
-@socketio.on('leave', namespace='/test')
-def leave(message):
-	leave_room(message['room'])
-	session['receive_count'] = session.get('receive_count', 0) + 1
-	emit('my_response',
-		{'data': 'In rooms: ' + ', '.join(rooms()),
-		'count': session['receive_count']})
-
-
-@socketio.on('close_room', namespace='/test')
-def close(message):
-	session['receive_count'] = session.get('receive_count', 0) + 1
-	emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
-	'count': session['receive_count']},
-	room=message['room'])
-	close_room(message['room'])
-
-
-@socketio.on('my_room_event', namespace='/test')
-def send_room_message(message):
-	session['receive_count'] = session.get('receive_count', 0) + 1
-	emit('my_response',
-		{'data': message['data'], 'count': session['receive_count']},
-		room=message['room'])
-
-
-@socketio.on('disconnect_request', namespace='/test')
-def disconnect_request():
-	session['receive_count'] = session.get('receive_count', 0) + 1
-	emit('my_response',
-		{'data': 'Disconnected!', 'count': session['receive_count']})
-	disconnect()
-
-
-@socketio.on('my_ping', namespace='/test')
-def ping_pong():
-	emit('my_pong')
-
-
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect', namespace='/main')
 def test_connect():
-	#global thready
-	#with thread_lock:
-	#	if thready is None:
-	#		thready = socketio.start_background_task(target=background_thread)
-	#emit('my_response', {'data': 'Connected', 'count': 0})
-        socketio.emit('my_response', {'data': 'Connected', 'count': 0})
+    socketio.emit('status', {'data': 'Connected'})
+	print('Client connected')
 
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect', namespace='/main')
 def test_disconnect():
-	print('Client disconnected', request.sid)
+	print('Client disconnected')
 
 def flaskThread():
-    #app.run(host='0.0.0.0.',port=8666)
     socketio.run(app,host=this.server_address,port=this.server_port)
 
 
@@ -139,26 +58,15 @@ def plugin_stop():
 	"""
 	EDMC is closing
 	"""
-	with thread_lock:
-		if thready is not None:
-			print "stop threads"
-			thready.stop()
-			flask_thread.stop()
+    if flask_thread is not None:
+        flask_thread.stop()
 	print "MissionReport unloaded!"
 
 def journal_entry(cmdr, is_beta, system, station, entry, state):
-    print "RESPONSE: **********************" 
-    print str(entry)
-    print "RESPONSE: **********************" 
-    #socketio_c.emit("my_response",json.loads(entry))
-    socketio.emit("event",{'data': entry, 'count': 0},namespace='/test') 
+    socketio.emit("journal",{'data': entry},namespace='/main') 
 
 def cmdr_data(data, is_beta):
-    print "RESPONSE: **********************"
-    print str(data) 
-    print "RESPONSE: **********************"
-    #socketio_c.emit("my_response",json.loads(data)) 
-    socketio.emit("event",{'data': data, 'count': 0},namespace='/test') 
+    socketio.emit("cmdr",{'data': data},namespace='/main') 
 
 
 
